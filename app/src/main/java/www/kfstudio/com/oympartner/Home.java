2f14d23d5f1819495464a9fd9e26b05842a39e9f
kashaf12 <kashafaahmed@gmail.com>
2019-06-16 19:06:52 +0530

    Initial commit

---
+++ app/src/main/java/www/kfstudio/com/oympartner/Home.java
@@ -0,0 +1,32 @@
+package www.kfstudio.com.oympartner;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class Home extends AppCompatActivity {
+}
